153563948fcfb247b7c3c6c7805b97f19cc9bb7e
sajeewa0825 <sajeewa0825@gmail.com>
2024-01-17 14:17:30 +0530

    cretae email.js file

---
+++ manager/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer")
+require('dotenv').config();
+
+const sendMail = async (to, subject, text) =>{
+    var transport = nodemailer.createTransport({
+        host: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PW
+        }
+      });
+
+    await transport.sendMail({
+        to:to,
+        from:process.env.EMAIL,
+        text:text,
+        subject:subject
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+module.exports = sendMail;
